0918dc40f19d659c503d88ba2e5b2325f64a7f1a
Jeff-Salvant <salvant3jeff@gmail.com>
2022-09-05 23:40:16 -0400

    Define basic validation function

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const pwd = document.getElementById("pwd");
 const pwdConfirm = document.getElementById("pwd-confirm");
 const errorMessage = document.querySelector('.one');
 const title = document.querySelector('.title');
+
+function validate(e) {
+    e.preventDefault();
+    if (pwd.value !== pwdConfirm.value) {
+      pwd.classList.add('invalid');
+      pwdConfirm.classList.add('invalid');
+      errorMessage.setAttribute('style', 'opacity: 1;');
+    } else {
+      pwd.classList.remove('invalid');
+      pwdConfirm.classList.remove('invalid');
+      errorMessage.setAttribute('style', 'opacity: 0;');
+    }
+}
