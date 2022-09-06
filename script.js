73cb1ec7dd41e8784aed387a9c000ce24a67a904
Jeff-Salvant <salvant3jeff@gmail.com>
2022-09-05 23:39:37 -0400

    Select desired html elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const pwd = document.getElementById("pwd");
+const pwdConfirm = document.getElementById("pwd-confirm");
+const errorMessage = document.querySelector('.one');
+const title = document.querySelector('.title');
